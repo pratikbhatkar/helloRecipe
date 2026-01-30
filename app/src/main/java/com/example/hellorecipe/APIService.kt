22777b7500562e9801e0be470e58e5ac51e600cb
pratikbhatkar <pratikbhatkar.engr@gmail.com>
2026-01-30 22:21:32 +0530

    added val recipeService to create retrofit : APIService

--- app/src/main/java/com/example/hellorecipe/APIService.kt
+++ app/src/main/java/com/example/hellorecipe/APIService.kt
@@ -1,9 +1,13 @@
 package com.example.hellorecipe
 
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 private val retrofit = Retrofit.Builder().baseUrl("www.themealdb.com/api/json/v1/1/")
+    .addConverterFactory(GsonConverterFactory.create()).build()
+
+val recipeService = retrofit.create(APIService::class.java)
 
 interface APIService{
     // this sends the http request to the server/API with the url
