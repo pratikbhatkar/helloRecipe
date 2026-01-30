67ae3eb212b14ff5c9e3e774f24fae95e6654b99
pratikbhatkar <pratikbhatkar.engr@gmail.com>
2026-01-30 22:09:51 +0530

    added APIservice interface

--- app/src/main/java/com/example/hellorecipe/APIService.kt
+++ app/src/main/java/com/example/hellorecipe/APIService.kt
@@ -1,2 +1,9 @@
 package com.example.hellorecipe
 
+import retrofit2.http.GET
+
+interface APIService{
+    // this sends the http request to the server/API with the url
+    @GET("categories.php") // we are sending request to categories.php file
+    suspend fun getCategories(): CategoriesResponse
+}
