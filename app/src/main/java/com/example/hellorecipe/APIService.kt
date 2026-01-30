c44c7dd84dd23adf9026ca83138bcdc67b2b270b
pratikbhatkar <pratikbhatkar.engr@gmail.com>
2026-01-30 22:16:44 +0530

    created a baseURL by Retrofit.Builder()

--- app/src/main/java/com/example/hellorecipe/APIService.kt
+++ app/src/main/java/com/example/hellorecipe/APIService.kt
@@ -1,6 +1,9 @@
 package com.example.hellorecipe
 
+import retrofit2.Retrofit
 import retrofit2.http.GET
+
+private val retrofit = Retrofit.Builder().baseUrl("www.themealdb.com/api/json/v1/1/")
 
 interface APIService{
     // this sends the http request to the server/API with the url
